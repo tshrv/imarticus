36c2a6a301add8ff2787f50df03a6be89eaddce6
Tushar <tusharsrivastava162@gmail.com>
2022-06-08 21:10:19 +0530

    base models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False)
+    thumbnail = models.ImageField(upload_to='course_thumbnails/', null=False, blank=False)
+
+    class Meta:
+        db_table = 'courses'
+        ordering = ('title',)
+    
+    def __str__(self) -> str:
+        return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=False, null=False)
+    title = models.CharField(max_length=255, blank=False, null=False)
+
+    class Meta:
+        db_table = 'modules'
+        ordering = ('title',)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Lecture(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, blank=False, null=False)
+    duration = models.PositiveIntegerField(blank=False, null=False)
+
+    class Meta:
+        db_table = 'lectures'
+        ordering = ('title',)
+
+    def __str__(self) -> str:
+        return self.title
