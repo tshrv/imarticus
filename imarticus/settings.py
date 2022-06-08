40b40ca3f1c183665a3fde638a6cdf1013adbdac
Tushar <tusharsrivastava162@gmail.com>
2022-06-08 23:34:16 +0530

    prod db

--- imarticus/settings.py
+++ imarticus/settings.py
@@ -92,15 +92,15 @@ WSGI_APPLICATION = 'imarticus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # prod db
-# DATABASES={}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES={}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
